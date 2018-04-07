8f01f34b99f74422ba107d43f631977f952680b5
kariyenas <apurvamayank@hotmail.com>
2018-04-07 14:46:54 +0530

    required files added

--- lib/dummy_data.rb
+++ lib/dummy_data.rb
@@ -1,14 +1,8 @@
-module DummyData
-  def self.give_details
-    puts 'Enter the mysql username'
-    username = gets.chomp
-    puts 'Enter the mysql password'
-    pass = gets.chomp
-    puts 'Enter database name'
-    db_name = gets.chomp
-    dbc = DataBaseConnector.new(username, pass, db_name)
-    dbc.make_it_dummy
+require 'dummy_email/database_connector'
+# The module is executed in the bin file
+module DummyEmail
+  def self.give_details(username, password, db_name, table_name, column_name)
+    dbc = DataBaseConnector.new(username, password, db_name)
+    dbc.dummy_email(table_name, column_name)
   end
 end
-
-load 'database_operation/database_connector.rb'
